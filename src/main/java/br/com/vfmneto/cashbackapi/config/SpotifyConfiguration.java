f3a4f4e6902ee13a339fadfefe2fe6c5fce7b3db
vfmneto <vfmneto@gmail.com>
2019-02-05 16:05:30 -0300

    Parametrizado as chaves de acesso API do Spotify.

--- src/main/java/br/com/vfmneto/cashbackapi/config/SpotifyConfiguration.java
+++ src/main/java/br/com/vfmneto/cashbackapi/config/SpotifyConfiguration.java
@@ -4,6 +4,7 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +13,14 @@ import java.io.IOException;
 @Configuration
 public class SpotifyConfiguration {
 
+    @Value("${application.spotify.clientId}") String clientId;
+    @Value("${application.spotify.clientSecret}") String clientSecret;
+
     @Bean
     public SpotifyApi spotifyApi() throws IOException, SpotifyWebApiException {
         SpotifyApi.Builder builder = new SpotifyApi.Builder();
-        SpotifyApi spotifyApi = builder.setClientId("90ff7c96027c4aa4953ac55d46517f4f")
-                                       .setClientSecret("664cbb31787449ca960af5d528de552a")
+        SpotifyApi spotifyApi = builder.setClientId(clientId)
+                                       .setClientSecret(clientSecret)
                                        .build();
         ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
         ClientCredentials clientCredentials = clientCredentialsRequest.execute();
